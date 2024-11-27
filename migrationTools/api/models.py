0878ea29dab68d99e0a332c3c25b6a535f2e236a
fmaulanaaaaa <fajar.maulana@sirclo.com>
2024-11-27 16:24:51 +0700

    update token field

--- migrationTools/api/models.py
+++ migrationTools/api/models.py
@@ -7,7 +7,7 @@ class Users(AbstractUser):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
         ('Viewer', 'Viewer')
-    ]    
+    ]
     id = models.CharField(max_length=5, primary_key=True, editable=False)
     fullname = models.CharField(max_length=40)
     username = models.CharField(max_length=15, unique=True)
@@ -22,25 +22,25 @@ class Users(AbstractUser):
     @property
     def is_anonymous(self):
         return False
-    
+
     @property
     def is_authenticated(self):
         return True
-    
+
     def __unicode__(self):
-        return self.fullname 
-    
+        return self.fullname
+
     def save(self, *args, **kwargs):
         if not self.pk:
             count = Users.objects.count() + 1
             self.id = 'RK{:03d}'.format(count)
-        super().save(*args, **kwargs)   
+        super().save(*args, **kwargs)
 
 class repoIntegration(models.Model):
     id = models.CharField(max_length=5, primary_key=True, editable=False)
     name = models.CharField(max_length=30)
     username = models.CharField(max_length=30)
-    token = models.CharField(max_length=150, null=True)
+    token = models.CharField(max_length=400, null=True)
     branch = models.CharField(max_length=20, null=True)
     repo_url = models.CharField(max_length=100)
     author = models.ForeignKey(Users, on_delete=models.SET_NULL, null=True, to_field='username')
@@ -55,7 +55,7 @@ class repoIntegration(models.Model):
         if not self.pk:
             count = repoIntegration.objects.count() + 1
             self.id = 'RP{:03d}'.format(count)
-        super().save(*args, **kwargs)        
+        super().save(*args, **kwargs)
 
 class migrationConfig(models.Model):
     id = models.CharField(max_length=6, primary_key=True, editable=False)
@@ -66,21 +66,21 @@ class migrationConfig(models.Model):
     db_name = models.CharField(max_length=20, null=True)
     db_password = models.CharField(max_length=400, null=True)
     author = models.ForeignKey(Users, on_delete=models.SET_NULL, null=True, to_field='username')
-    
+
     def encrypt(self,raw_password):
         self.db_password=encrypt_data(raw_password)
 
     def decrypt(self):
-        self.db_password=decrypt_data(self.db_password)  
+        self.db_password=decrypt_data(self.db_password)
 
     def save(self, *args, **kwargs):
         if not self.pk:
             count = migrationConfig.objects.count() + 1
             self.id = 'CFG{:03d}'.format(count)
-        super().save(*args, **kwargs)             
+        super().save(*args, **kwargs)
 
 class migrationData(models.Model):
-    id = models.CharField(max_length=12, primary_key=True, editable=False)    
+    id = models.CharField(max_length=12, primary_key=True, editable=False)
     sql_query = models.TextField(max_length=250)
     status_query = models.CharField(max_length=10, null=True)
     engineer_name = models.CharField(max_length=20)
@@ -97,4 +97,4 @@ class migrationData(models.Model):
             today = datetime.now()
             formatted_date = today.strftime("%Y%m%d")
             self.id = formatted_date + '-{:03d}'.format(count)
-        super().save(*args, **kwargs)     
+        super().save(*args, **kwargs)
